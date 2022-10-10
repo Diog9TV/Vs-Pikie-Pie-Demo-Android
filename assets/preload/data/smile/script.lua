0d2ad6e10194d7ca59ece3240309e2cd83bd2efb
Diog9TV <89103529+Diog9TV@users.noreply.github.com>
2022-10-10 14:02:44 -0300

    Add files via upload

---
+++ assets/preload/data/smile/script.lua
@@ -0,0 +1,20 @@
+function onStepHit()
+	if curStep == 276 then
+	   triggerEvent('Play Animation', 'hey', 'BF');
+	end
+	if curStep == 146 then
+	   triggerEvent('Play Animation', 'howdy', 'Dad');
+    end
+	if curStep == 178 then
+	   triggerEvent('Play Animation', 'howyadoin', 'Dad');
+    end
+    if curStep == 619 then
+       triggerEvent('Play Animation', 'whatsup', 'Dad');
+    end
+    if curStep == 724 then
+       triggerEvent('Play Animation', 'hello', 'Dad');
+    end
+    if curStep == 1197 then
+       triggerEvent('Flash Camera', '0.5', '');
+    end
+end
